d4389d1f28ee53a05a27f3c86a2dd20d3e8a2ed9
Jordan Rob <jordanrob709@gmail.com>
2024-05-22 16:45:00 +0300

    added property validation test

--- Python/TirePressureMonitoringSystem/test_tire_pressure_monitoring.py
+++ Python/TirePressureMonitoringSystem/test_tire_pressure_monitoring.py
@@ -7,3 +7,24 @@ class AlarmTest(unittest.TestCase):
     def test_alarm_is_off_by_default(self):
         alarm = Alarm()
         assert not alarm.is_alarm_on
+
+    def test_property_validation(self):
+        alarm = Alarm()
+
+        assert alarm._high_pressure_threshold == 21
+        assert alarm._low_pressure_threshold == 17
+        assert alarm._is_alarm_on ==False
+        assert not alarm._sensor == None
+
+
+# Possible Test cases
+    
+# validate properties that we need:
+# loW_pressure_threshold
+# high_pressure_threshold
+# sensor
+# is_alarm_on = False
+    
+# check() tests
+# is_alarm_on  psi_pressure_value < low_pressure_threshold
+# high_pressure_threshold < psi_pressure_value
